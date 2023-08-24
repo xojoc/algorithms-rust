cb7fc7db82627ea908d3d324eed3db3454c22707
Alexandru Cojocaru <hi@xojoc.pw>
2023-08-24 11:54:21 +0200

    s

---
+++ Makefile
@@ -0,0 +1,16 @@
+nothing:
+
+
+
+build:
+	cargo build
+
+run:
+	cargo run
+
+test:
+	cargo test
+
+lint:
+	cargo clippy
+
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+fn binary_search<T: PartialOrd>(elements: &[T], needle: T) -> usize {
+    return 0;
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
