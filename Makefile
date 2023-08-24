6a19b8232496a7db607427fab2845fb1e5df5d99
Alexandru Cojocaru <hi@xojoc.pw>
2023-08-24 17:21:18 +0200

    s

--- Makefile
+++ Makefile
@@ -9,8 +9,12 @@ run:
 	cargo run
 
 test:
-	cargo test
+	cargo test # -- --nocapture
 
 lint:
 	cargo clippy
 
+push:
+	git commit -m s -a
+	git push
+	
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,41 @@
-fn binary_search<T: PartialOrd>(elements: &[T], needle: T) -> usize {
-    return 0;
+mod sort;
+
+fn _binary_search_recursive<T: PartialOrd + std::fmt::Debug>(
+    haystack: &[T],
+    needle: T,
+    from: usize,
+    to: usize,
+) -> Option<usize> {
+    if from > to {
+        return None;
+    }
+    let middle = from + (to - from) / 2;
+    println!(
+        "Haystack: {:?}, needle: {:?}, from: {:?}, to: {:?}, middle: {:?}",
+        &haystack[from..=to],
+        needle,
+        from,
+        to,
+        middle
+    );
+
+    if haystack[middle] == needle {
+        return Some(middle);
+    } else if haystack[middle] > needle {
+        return _binary_search_recursive(haystack, needle, from, middle - 1);
+    } else {
+        return _binary_search_recursive(haystack, needle, middle + 1, to);
+    }
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn binary_search_recursive<T: PartialOrd + std::fmt::Debug>(
+    haystack: &[T],
+    needle: T,
+) -> Option<usize> {
+    if haystack.len() == 0 {
+        return None;
+    }
+    return _binary_search_recursive(haystack, needle, 0, haystack.len() - 1);
 }
 
 #[cfg(test)]
@@ -11,8 +43,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_binary_search() {
+        assert_eq!(binary_search_recursive(&[], 3), None);
+        assert_eq!(binary_search_recursive(&[1, 2, 3], 4), None);
+        assert_eq!(binary_search_recursive(&[1, 2, 3, 4], 4), Some(3));
+        assert_eq!(binary_search_recursive(&[1, 2, 3, 4, 5], 4), Some(3));
     }
 }
